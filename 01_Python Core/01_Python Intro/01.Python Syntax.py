765f303c56256e4ae58423cc9a031cdefd73da2c
Md. Zehadul Islam <154805881+mdzehadulislam8@users.noreply.github.com>
2026-01-24 22:27:37 +0600

    Add initial Python syntax examples and structure to the repository

---
+++ 01_Python Core/01_Python Intro/01.Python Syntax.py
@@ -0,0 +1,67 @@
+# Syntax
+
+# What is Syntax in Python?
+# Syntax refers to the set of rules that defines the combinations of symbols
+# that are considered to be correctly structured programs in that language.
+
+# Execute a simple print statement in Python
+
+print("Hello, World!") # <-- Correct Syntax
+
+
+# Syntax Error Example
+
+## print("Hello, World") <---- Error: Missing closing parenthesis
+
+# Sum
+print(5 + 3) # <-- Correct Syntax answer is 8
+
+# Minus
+print(10 - 4) # <-- Correct Syntax answer is 6
+
+
+
+
+
+# Indentation
+
+# Indentation is crucial in Python as it defines the blocks of code.
+
+# Correct Indentation Example:
+
+if 5 > 2:
+    print("Five is greater than two!")  # <-- Correct Indentation
+# Incorrect Indentation Example
+# if 5 > 2:
+# print("Five is greater than two!")  # <-- Error: IndentationError
+
+# Loop with Indentation
+while True:
+    print("This will print forever until you stop it!")  # <-- Correct Indentation
+    break  # To prevent infinite loop in this example
+# Incorrect Indentation Example
+# while True:
+# print("This will print forever until you stop it!")  # <-- Error: IndentationError
+# break  # To prevent infinite loop in this example
+
+
+
+
+# Comments
+
+# ----> Comments are used to explain code and are ignored by the Python interpreter. <------
+
+# Single-line comment
+# Single-line comment are created using the hash symbol (#).
+# This is a single-line comment in Python <-----
+
+
+# Multi-line comment
+
+"""
+This is also a way to create
+a multi-line comment using      <------- Multiple lines Comment
+triple double quotes.
+"""
+
+
